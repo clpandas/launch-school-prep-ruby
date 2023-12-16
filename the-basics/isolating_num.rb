b1fc5cb8ed1ed3c2153f19d66e02a66941f655bb
Crystal Lor <crystalalor@gmail.com>
2023-12-16 12:14:49 +0000

    Utilizing string interpolation to output the results of arithmetic operations on a 4 digit number.

--- the-basics/isolating_num.rb
+++ the-basics/isolating_num.rb
@@ -3,7 +3,7 @@ hundreds = 3141 % 1000 / 100
 tens = 3141 % 100 / 10
 ones = 3141 % 10
 
-puts thousands
-puts hundreds 
-puts tens
-puts ones
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
