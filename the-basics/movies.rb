89577d193148cb8d06e638ea843f47d33fa7d40c
Crystal Lor <crystalalor@gmail.com>
2023-12-17 04:43:54 +0000

    An example with Ruby's new syntax for creating hashes by using comma separated key-value pairs.

--- the-basics/movies.rb
+++ the-basics/movies.rb
@@ -1,8 +1,8 @@
-movies = {  :jaws => 1975,
-            :the_day_after_tomorrow => 2004,
-            :frozen => 2013,
-            :shrek => 2001,
-            :raiders_of_the_last_ark => 1981 }
+movies = {  jaws: 1975,
+            the_day_after_tomorrow: 2004,
+            frozen: 2013,
+            shrek: 2001,
+            raiders_of_the_last_ark: 1981 }
             
 puts movies [:jaws]
 puts movies [:the_day_after_tomorrow]
