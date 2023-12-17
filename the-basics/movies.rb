4da1f0ad31cfaeb207552c5c37dfb87cf6df765e
Crystal Lor <crystalalor@gmail.com>
2023-12-17 04:48:55 +0000

    A comment regarding how the comma separated key-value pair syntax only works when using symbols as a key.

--- the-basics/movies.rb
+++ the-basics/movies.rb
@@ -3,6 +3,8 @@ movies = {  jaws: 1975,
             frozen: 2013,
             shrek: 2001,
             raiders_of_the_last_ark: 1981 }
+
+# the comma separated key-value pair syntax only works when using symbols as a key.   
             
 puts movies [:jaws]
 puts movies [:the_day_after_tomorrow]
