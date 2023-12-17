5863483616528045dac860d4f5fdc8799377f2f0
Crystal Lor <crystalalor@gmail.com>
2023-12-17 04:39:32 +0000

    An example of using a hash to store a list of movie titles and the year they came out. Then printing out the year of each movie onto the console using the puts command.

---
+++ the-basics/movies.rb
@@ -0,0 +1,11 @@
+movies = {  :jaws => 1975,
+            :the_day_after_tomorrow => 2004,
+            :frozen => 2013,
+            :shrek => 2001,
+            :raiders_of_the_last_ark => 1981 }
+            
+puts movies [:jaws]
+puts movies [:the_day_after_tomorrow]
+puts movies [:frozen]
+puts movies [:shrek]
+puts movies [:raiders_of_the_last_ark]
