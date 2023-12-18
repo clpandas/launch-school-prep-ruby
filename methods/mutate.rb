323cd2f87bece20c0d56691480bf23815a95e227
Crystal Lor <crystalalor@gmail.com>
2023-12-18 05:27:41 +0000

    An additional example of a method definition that does not mutate the argument.

--- methods/mutate.rb
+++ methods/mutate.rb
@@ -8,3 +8,14 @@ end
 p "Before mutate method: #{a}"
 mutate(a)
 p "After mutate method: #{a}"
+
+b = [1, 2, 3]
+
+# Example of a method definition that does not mutate the argument
+def no_mutate(array)
+  array.last
+end
+
+p "Before no_mutate method: #{b}"
+no_mutate(b)
+p "After no_mutate method: #{b}"
