0e438a16c21c230390f069dc64a2939cb040c598
Crystal Lor <crystalalor@gmail.com>
2024-01-02 07:43:14 +0000

    Manipulating a hash with element reference, element assignment, and the delete_if method.

---
+++ exercises/key_value_pair.rb
@@ -0,0 +1,18 @@
+h = {a:1, b:2, c:3, d:4}
+
+# 1. element reference
+p h[:b]
+
+#2. element assignment 
+h[:e] = 5
+p h
+
+#3 single line version
+h.delete_if { |k,v| v <3.5 }
+
+#3 multi-line version
+h.delete_if do |k,v|
+  v <3.5
+end
+
+p h
